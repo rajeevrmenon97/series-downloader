7ef4e92f701496784a1edfc52bc3ffcff0c3530e
Rajeev R Menon <rajeevrmenon97@live.com>
2018-04-20 00:43:16 +0530

    update urls

--- downloadseries/urls.py
+++ downloadseries/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name = 'home',),
+    url(r'^search', views.search, name = 'search',),
 ]
